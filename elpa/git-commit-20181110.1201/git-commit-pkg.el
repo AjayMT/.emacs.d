0f7801af0bc88d8541f466624e50906c81426246
Ajay MT <ajay.tatachar@gmail.com>
2018-11-10 18:30:27 -0600

    install neotree, update packages

---
+++ elpa/git-commit-20181110.1201/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20181110.1201" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "e19e8fb1ea0548fce89cd4fd2bc552ec71649df1" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
