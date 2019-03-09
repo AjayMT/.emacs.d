deb364b104a1e7a5fd74ab15db74af9a03567bbe
Ajay MT <ajay.tatachar@gmail.com>
2019-03-09 13:53:53 -0600

    update packages, install pretty-mode

--- elpa/dart-mode-20190125.209/dart-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dart-mode" "20190125.209" "Major mode for editing Dart files" '((emacs "24.5") (cl-lib "0.5") (dash "2.10.0") (flycheck "0.23") (s "1.10")) :stars '(#("81" 0 2 (font-lock-face paradox-star-face))) :commit "591b6fee1fbbe58450ab96c2ad17540d503c4abf" :keywords '("languages") :authors '(("Natalie Weizenbaum") ("Brady Trainor" . "mail@bradyt.com")) :maintainer '("Brady Trainor" . "mail@bradyt.com") :url "https://github.com/bradyt/dart-mode")
