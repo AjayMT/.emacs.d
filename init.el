627c9a900eac70cb6de15db1c1c2954f9faaf77e
Ajay MT <ajay.tatachar@gmail.com>
2022-04-06 17:13:21 -0500

    add ocp-indent and ocp-index

--- init.el
+++ init.el
@@ -102,14 +102,16 @@
   (load-theme 'basic t)
   (add-to-list 'default-frame-alist '(font . "Monaco 10")))
 
-;; merlin-mode
+;; ocaml packages
 (let ((opam-share (ignore-errors (car (process-lines "opam" "config" "var" "share")))))
   (when (and opam-share (file-directory-p opam-share))
-    ;; Register Merlin
     (add-to-list 'load-path (expand-file-name "emacs/site-lisp" opam-share))
+
+    (require 'ocp-index)
+    (require 'ocp-indent)
     (autoload 'merlin-mode "merlin" nil t nil)
-    ;; Automatically start it in OCaml buffers
+
     (add-hook 'tuareg-mode-hook 'merlin-mode t)
     (add-hook 'caml-mode-hook 'merlin-mode t)
-    ;; Use opam switch to lookup ocamlmerlin binary
+
     (setq merlin-command 'opam)))
